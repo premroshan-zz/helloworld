1bb20adc6251f2c72f7602e5e6e2aa6f2c6eecec
Prem Nair <premroshan@gmail.com>
2016-10-10 15:30:43 +0530

    changed to a regular java application. First commit of hello world

---
+++ helloworld/src/main/java/com/spring/helloworld/helloworld.java
@@ -0,0 +1,15 @@
+package com.spring.helloworld;
+
+public class helloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		if(args.length > 0 ){
+			System.out.println(args[0]);
+		}
+		else{
+			System.out.println("Hello World");
+		}
+	}
+
+}
